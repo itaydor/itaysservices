8854ceea3be367d6af338da7bc869df3cdc68b74
itaydor <108480612+itaydor@users.noreply.github.com>
2022-07-27 21:26:36 +0300

    @LoadBalanced was added to RestTemplate

--- customer/src/main/java/com/itayscode/customer/CustomerService.java
+++ customer/src/main/java/com/itayscode/customer/CustomerService.java
@@ -26,7 +26,7 @@ public class CustomerService {
         customerRepository.saveAndFlush(customer);
 
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerID}",
+                "http://FRAUD/api/v1/fraud-check/{customerID}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
