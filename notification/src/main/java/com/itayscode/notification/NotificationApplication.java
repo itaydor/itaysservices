c75f229a74bae5d32577d3e5901e94c3d5c41cbf
itaydor <108480612+itaydor@users.noreply.github.com>
2022-07-28 17:23:32 +0300

    adding commandLineRunner to test message pass from the exchange to the queue

--- notification/src/main/java/com/itayscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/itayscode/notification/NotificationApplication.java
@@ -1,14 +1,34 @@
 package com.itayscode.notification;
 
+import com.itayscode.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.itayscode.notification",
+                "com.itayscode.amqp",
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+        return args -> {
+            producer.publish(
+                    new Person("itay", 35),
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getNotificationQueue());
+        };
+    }
+
+    record Person(String name, int age){}
 }
